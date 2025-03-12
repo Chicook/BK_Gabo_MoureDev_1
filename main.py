3243415647c0695b2060e6cb04f6ee0d7f224361
Gabriel Revelo <gabriel.revelo@stockviz.com>
2025-03-12 12:14:09 -0500

    movimiento parabolico

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ clock = pygame.time.Clock()
 
 # variables
 cuadrado = pygame.Rect(0,HEIGHT//2,60,60)
-velocidad = -8
+velocidad = [3,-14]
 gravedad = 0.4
 
 
@@ -21,10 +21,18 @@ while True:
             quit()
 
     # code here
-    #cuadrado.x += velocidad[0]
-    cuadrado.y += velocidad
-    velocidad += gravedad
+    if cuadrado.y > HEIGHT-60:
+        velocidad[1] = 0
+        gravedad = 0
+
+    cuadrado.x += velocidad[0]
+    cuadrado.y += velocidad[1]
+    velocidad[1] += gravedad
     pygame.draw.rect(screen, (255, 255, 255), cuadrado)
+
+    if cuadrado.y > HEIGHT:
+        velocidad[1] = 0
+        gravedad = 0
 
     pygame.display.update()
     screen.fill((0, 0, 0))
