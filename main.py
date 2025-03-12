e8236ed872d2ad9ce7302a07d714d0caaa4e4500
Gabriel Revelo <gabriel.revelo@stockviz.com>
2025-03-12 11:44:27 -0500

    MRU

---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame
+from pygame.locals import *
+
+
+WIDTH, HEIGHT = 400, 500
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+
+# constants
+
+
+# variables
+cuadrado = pygame.Rect(0,HEIGHT//2,60,60)
+velocidad = 3
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            quit()
+
+    # code here
+    cuadrado.x += velocidad
+    pygame.draw.rect(screen, (255, 255, 255), cuadrado)
+
+    pygame.display.update()
+    screen.fill((0, 0, 0))
+    clock.tick(30)
